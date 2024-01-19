67f6ad7605c1780cfdc1d382a214c2992b7c9722
Rishi Anand <rishi.a@honeysys.com>
2024-01-19 19:02:41 +0530

    ts project initilizefd

---
+++ TsProjectOne/src/index.ts
@@ -0,0 +1,5 @@
+console.log('Welcome to typescript');
+console.log("RishiAnand");
+console.log("hi rishi")
+console.log("")
+console.log("Vishal")
---
+++ TsProjectOne/src/utils/log.ts
@@ -0,0 +1,3 @@
+export const Console = (message : string | number )=>{
+    console.log(message)
+}
